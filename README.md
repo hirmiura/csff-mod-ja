aecf287b53fc0457261e48f6d93a0bbf7c7de372
Hiroshi Miura <hirmiura@gmail.com>
2025-03-01 16:28:06 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# csff-mod-ja
+
+[Card Survival: Fantasy Forest] の私家版和訳  
+簡体字版乗っ取り方式
+
+## インストール方法
+
+1. [Ja.csv](
+https://raw.githubusercontent.com/hirmiura/csff-mod-ja/main/Jp.csv) をダウンロードし  
+`Card Survival Fantasy Forest/Card Survival - Fantasy Forest_Data/StreamingAssets/Localization/Cn.csv` に上書きコピーする。
+2. ゲームを起動し、言語設定を**簡体字**に変更する
+3. ゲームを再起動する
+
+## 翻訳手順忘備録
+
+```bash
+encsv.py -i 本体/Kr.csv -o En.csv
+csv2pot.py -i En.csv -o Ja.pot
+msginit --no-translator -l ja_JP.utf8 -i Ja.pot -o Ja.edit.po
+```
+
+`Ja.edit.po` を翻訳
+
+```bash
+msgfmt --statistics -o Ja.mo Ja.edit.po
+mo2csv.py -i Ja.mo -o Ja.csv
+```
+
+## マージ
+
+```bash
+msgmerge --no-fuzzy-matching --no-location --no-wrap --backup=t -U Ja.po new.pot
+msgcat --use-first --no-location --no-wrap -o Ja.po Ja.edit.po Ja.po
+```
+
+### git用に簡略化する
+
+```bash
+msgattrib --no-obsolete --no-location --no-wrap --sort-output -o - Ja.po \
+| grep -vE '^"(POT-Creation-Date|X-Generator):.*\\n"' \
+| sponge Ja.po
+```
+
+## ライセンス
+
+[MIT License] としています。  
+ご自由にお使いください。
+
+---
+
+[Card Survival: Fantasy Forest]: https://store.steampowered.com/app/2868860/Card_Survival_Fantasy_Forest/
+[MIT License]: https://opensource.org/license/mit/
---
+++ src/csff_mod_ja/encsv.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env -S python
+# SPDX-License-Identifier: MIT
+# Copyright 2025 hirmiura (https://github.com/hirmiura)
+"""任意言語のcsvファイルから英語版のcsvファイルを生成する。"""
+
+from __future__ import annotations
+
+import argparse
+import csv
+import os
+import sys
+
+
+def pargs() -> argparse.Namespace:
+    """コマンドライン引数を処理する
+
+    Returns:
+        argparse.Namespace: 処理した引数
+    """
+    parser = argparse.ArgumentParser(
+        description="任意言語のcsvファイルから英語版のcsvファイルを生成する。"
+    )
+    parser.add_argument(
+        "-i",
+        metavar="INPUT",
+        type=argparse.FileType("r", encoding="utf-8-sig"),
+        default="-",
+        help="入力ファイル。指定されなければ標準入力",
+    )
+    parser.add_argument(
+        "-o",
+        metavar="OUTPUT",
+        type=argparse.FileType("w", encoding="utf-8"),
+        default=sys.stdout,
+        help="出力ファイル。指定されなければ標準出力",
+    )
+    parser.add_argument("--version", action="version", version="%(prog)s 0.1.0")
+    args = parser.parse_args()
+    return args
+
+
+def main() -> int:
+    """メイン関数
+
+    Returns:
+        int: 成功時は0を返す
+    """
+    args = pargs()
+    reader = csv.reader(args.i)
+    writer = csv.writer(args.o)
+    try:
+        for row in reader:
+            writer.writerow([*row[0:2], row[1]])
+    except BrokenPipeError:
+        # パイプが切れた時のエラーを消す
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
+        return 1
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
